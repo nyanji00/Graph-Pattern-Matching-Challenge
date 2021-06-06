a7319ac7d2453ad168928944d90c101383bd0625
29din <rbwls5567@wafflestudio.com>
2021-06-06 18:41:59 +0900

    write todo list

--- src/backtrack.cc
+++ src/backtrack.cc
@@ -12,5 +12,21 @@ void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
                                 const CandidateSet &cs) {
   std::cout << "t " << query.GetNumVertices() << "\n";
 
-  // implement your code here.
+  // 1. Make DAG q_d
+  // 1-1. For DAG-Ordering, *Alway choose extendable vertex*
+  // 1-2. From M{ (u0, v0) } when CS{ (u0, v0), ... }
+
+  // 1-3. Choose next Candidate vertices... ex) u1 and u2 
+  // 1-4. ¤¤> By choosing from next Extendable Candidate... 
+  //        ex) N(u0, u1, v1) = {v2, v3}
+  // 		ex) N(u0, u2, v1) = {v4, v5, v6}
+  // 1-5. Choose what vertex will be next is your choice... (look PDF!)
+
+  // 2. Let D[u, v]=1 for all CS
+  
+  // 3. Use q_d and q_d^-1 alternatively, calculate D'[u, v] reculsively.
+
+  // 4. Make CS compact
+
+  // 5. Check Subgraph from CS
 }
