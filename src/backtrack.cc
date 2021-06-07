b15bf0dedd1792051348f855aefe769a1603fdef
sinyanji <wl77886@gmail.com>
2021-06-07 20:14:37 +0900

    write findRoot()

--- src/backtrack.cc
+++ src/backtrack.cc
@@ -4,15 +4,35 @@
  */
 
 #include "backtrack.h"
+#include <limits>
 
 Backtrack::Backtrack() {}
 Backtrack::~Backtrack() {}
 
+Vertex findRoot(const Graph &query, const CandidateSet &cs);
+
 void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
                                 const CandidateSet &cs) {
+
   std::cout << "t " << query.GetNumVertices() << "\n";
 
-  // Must not use pruning by failing sets
+  Vertex root = findRoot(query, cs);
 }
 
-void Backtrack::tracking(const CandidateSet &cs, )
+Vertex findRoot(const Graph &query, const CandidateSet &cs) {
+  size_t numVertice = query.GetNumVertices();
+  Vertex root = (Vertex) numVertice;
+  double minNum = std::numeric_limits<double>::max();
+
+  for(Vertex i=0; i<numVertice; i++) {
+    size_t candidateSize = cs.GetCandidateSize(i);
+    size_t degree = query.GetDegree(i);
+    double d = (double)candidateSize / (double)degree;
+    if(d < minNum) {
+      root = i;
+      minNum = d;
+    }
+  }
+
+  return root;
+}
