c7bce80590432e1fdd91c3c77d3f61429a9a405e
29din <rbwls5567@wafflestudio.com>
2021-06-07 23:04:21 +0900

    retry write initialParents()

--- src/backtrack.cc
+++ src/backtrack.cc
@@ -6,6 +6,7 @@
 #include "backtrack.h"
 #include <limits>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -15,9 +16,13 @@ Backtrack::~Backtrack() {}
 vector<Vertex> M;
 vector<Vertex> M_search;
 
+vector<vector<Vertex>> parents;
+
+vector<Vertex> findParents(Vertex u);
+
 void tracking(const Graph &query, const CandidateSet &cs);
 vector<Vertex> findNext(const Graph &query, Vertex r);
-Vertex calculateCm(Vertex u, std::vector<Vertex> up, const CandidateSet &cs);
+Vertex calculateCm(Vertex u, vector<Vertex> up, const CandidateSet &cs);
 Vertex findRoot(const Graph &query, const CandidateSet &cs);
 Vertex firstCandidate(const Graph &query, const CandidateSet &cs, Vertex u);
 
@@ -33,6 +38,22 @@ void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
 
   /* find root */
   Vertex root = findRoot(query, cs);
+
+  /* initialize Parents */
+  initializeParents(root, numVertice, query);
+  vector<vector<Vertex>>::iterator iter;
+	int i=0;
+  for(iter = parents.begin(); iter != parents.end(); iter++) {
+	  vector<Vertex> idx = *iter;
+		cout << i << " ";
+		vector<Vertex>::iterator initer;
+		for(initer = idx.begin(); initer != idx.end(); initer++) {
+			cout << *initer << " ";
+		}
+		cout << endl;
+		i++;
+  }
+  
 
   // M_search.insert(upper_bound(M_search.begin(), M_search.end(), 161), 161);
   // cout << firstCandidate(query, cs, 3) << endl;
@@ -56,16 +77,56 @@ Vertex firstCandidate(const Graph &query, const CandidateSet &cs, Vertex u) {
   return (Vertex)(-1);
 }
 
-std::vector<Vertex> findNext(const Graph &query) {
+void initializeParents(Vertex root, size_t numVertice, const Graph &query) {
+  parents.resize(numVertice);
+  vector<Vertex> childs;
+  queue<Vertex> q;
+  
+  vector<bool> visited(numVertice, false);
+
+  size_t start_offset = query.GetNeighborStartOffset(root);
+  size_t end_offset = query.GetNeighborEndOffset(root);
+  for(size_t i = start_offset; i < end_offset; i++) {
+		childs.push_back(query.GetNeighbor(i));
+  }
+
+  visited[root] = true;
+  vector<Vertex>::iterator iter;
+  for(iter = childs.begin(); iter != childs.end(); iter++) {
+    parents[*iter].push_back(root);
+		q.push(*iter);
+  }
+  
+  while(!q.empty()) {
+    Vertex next = q.front();
+    q.pop();
+    
+		size_t start_offset = query.GetNeighborStartOffset(next);
+  	size_t end_offset = query.GetNeighborEndOffset(next);
+
+  	for(size_t i = start_offset; i < end_offset; i++) {
+			Vertex child = query.GetNeighbor(i);
+			if (!visited[child]) childs.push_back(child);
+  	}
+
+  	for(iter = childs.begin(); iter != childs.end(); iter++) {
+    	parents[*iter].push_back(next);
+			q.push(*iter);
+  	}
+  }
+}
+
+vector<Vertex> findParents(Vertex u) {return parents[u];}
+vector<Vertex> findNext(const Graph &query) {
   
 }
 
-std::vector<Vertex> calculateCm(Vertex u, std::vector<Vertex> up_set, const Graph &data, const Graph &query, const CandidateSet &cs) {
+vector<Vertex> calculateCm(Vertex u, vector<Vertex> up_set, const Graph &data, const Graph &query, const CandidateSet &cs) {
 	
 	// complete parent vertex set
-	std::vector<Vertex> vp_set;
+	vector<Vertex> vp_set;
 
-	std::vector<Vertex>::iterator iter;
+	vector<Vertex>::iterator iter;
 	for(iter = up_set.begin(); iter != up_set.end(); iter++) {
 		vp_set[*iter] = M[*iter];
 	}
@@ -73,8 +134,8 @@ std::vector<Vertex> calculateCm(Vertex u, std::vector<Vertex> up_set, const Grap
 	// get possible candidates of u
 	size_t size_cs_u = cs.GetCandidateSize(u);
 	size_t size_cm_u = 0;
-	std::vector<Vertex> cs_u(size_cs_u);
-	std::vector<Vertex> cm_u;
+	vector<Vertex> cs_u(size_cs_u);
+	vector<Vertex> cm_u;
 
 	for(size_t i = 0; i < size_cs_u; i++) {
 
@@ -96,7 +157,7 @@ std::vector<Vertex> calculateCm(Vertex u, std::vector<Vertex> up_set, const Grap
 Vertex findRoot(const Graph &query, const CandidateSet &cs) {
   size_t numVertice = query.GetNumVertices();
   Vertex root = (Vertex)numVertice;
-  double minNum = std::numeric_limits<double>::max();
+  double minNum = numeric_limits<double>::max();
 
   for(size_t i=0; i<numVertice; i++) {
     size_t candidateSize = cs.GetCandidateSize(i);
